811b45c682e19add8641990454940398517d5ca4
Bipul khanal <102456900+Bipulx@users.noreply.github.com>
2022-11-12 10:33:05 +0545

    Add files via upload

---
+++ click.js
@@ -0,0 +1,9 @@
+var box = document.getElementById(box)
+ function double(){
+ let img = document.querySelector('.fas');
+ img.classList.add('show')
+ setTimeout(() => {
+    img.classList.remove('show');
+  }, 1200);
+}
+  
